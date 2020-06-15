b67a0eab4da3773da148c67358dfd7d5d3992eb1
ZatX <zatxmapki@gmail.com>
2020-06-15 19:53:09 +0200

    FIXED

--- Slider/save.js
+++ Slider/save.js
@@ -1,42 +1,61 @@
 function createCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	var d = new Date();
+	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+	var expires = 'expires=' + d.toUTCString();
+	document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
 }
 
-function readCookie(cname){
-    var name = cname + '=';
-    var decodedCookie = decodeURIComponent(document.cookie)
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length;i++){
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if(c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function readCookie(cname) {
+	var name = cname + '=';
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return '';
 }
 
-function saveCookies(cookiesArr, cookiesNames, ex){
-    console.log('saving...');
-    for(let i = 0; i < cookiesArr.length; i++){
-        createCookie(cookiesNames[i], cookiesArr[i], ex);
-    }
-    
-    //createCookie('fullScore', fullScore, '30');
-    //createCookie('bestScores', bestScores, '30');
-    console.log('saved');
+function saveCookies(cookiesArr, cookiesNames, ex) {
+	console.log('saving...', cookiesArr.length);
+	for (let i = 0; i < cookiesArr.length; i++) {
+		console.log(cookiesArr[i]);
+		if (cookiesArr[i] == 'NaN' || cookiesArr[i] == 'Undifined') {
+			if (cookieNames[i] == 'maxScore') {
+				cookieArr[i] = 10;
+			} else {
+				cookiesArr[i] = 0;
+			}
+		}
+		createCookie(cookiesNames[i], cookiesArr[i], ex);
+	}
 
-    //if(maxScore == '' || maxScore == 'NaN'){maxScore = 0;}
-    //if(fullScore == '' || fullScore == 'NaN'){fullScore = 0;}
-    //if(bestScores == '' || bestScores == 'NaN'){bestScores = 0;}
+	//createCookie('fullScore', fullScore, '30');
+	//createCookie('bestScores', bestScores, '30');
+	console.log('saved');
+
+	//if(maxScore == '' || maxScore == 'NaN'){maxScore = 0;}
+	//if(fullScore == '' || fullScore == 'NaN'){fullScore = 0;}
+	//if(bestScores == '' || bestScores == 'NaN'){bestScores = 0;}
 }
 
-function loadCookie(cname){
-    cookie = readCookie(cname)
-    return cookie
+function loadCookie(cname) {
+	cookName = cname;
+	cookie = readCookie(cname);
+	console.log(cookName);
+	if (cookie != parseInt(cookie, 10)) {
+		if (cname == 'maxScore') {
+			cookie = 10;
+			return cookie;
+		} else {
+			cookie = 0;
+			return cookie;
+		}
+	}
+	return cookie;
 }
--- Slider/sketch.js
+++ Slider/sketch.js
@@ -1,135 +1,145 @@
 var width = 600;
-var barVal = 0 ;
+var barVal = 0;
 holding = false;
 go = 'right';
-scores = []
-spots = []
+scores = [];
+spots = [];
 main = 0;
 maxScore = 10;
 fullScore = 0;
 bestScores = 0;
-barX = width/2 - 150;
+barX = width / 2 - 150;
 barY = 200;
 
 function setup() {
-  createCanvas(600, 600);
-  frameRate(60);
+	createCanvas(600, 600);
+	frameRate(60);
 
-  spot = new spot(random((width/2 - 150) + 25 + 20, (width/2 - 150) + 25 + 250), 225);
-  spots.push(spot);
+	spot = new spot(random(width / 2 - 150 + 25 + 20, width / 2 - 150 + 25 + 250), 225);
+	spots.push(spot);
 
-  btn = createButton('Save');
-  btn.mousePressed(pressy);
-  btn.position(width - 50, 2);
+	btn = createButton('Save');
+	btn.mousePressed(pressy);
+	btn.position(width - 50, 2);
 
-  maxScore = int(loadCookie('maxScore'))
-  fullScore = int(loadCookie('fullScore'))
-  bestScores = int(loadCookie('bestScores'))
+  if(document.cookie != ''){
+    console.log('loadigg cookies')
+    maxScore = loadCookie('maxScore');
+    fullScore = loadCookie('fullScore');
+    bestScores = loadCookie('bestScores');
+    maxScore = int(maxScore);
+    fullScore = int(fullScore);
+    bestScores = int(bestScores);
+  } else {
+    maxScore = 10;
+    fullScore = 0;
+    bestScores = 0;
+  }
 }
 
 function draw() {
+	background(30, 30, 40);
+	mouseOverBar = mouseX >= barX && mouseX <= barX + 300 && mouseY >= barY && mouseY <= barY + 50;
 
-  background(30, 30, 40);
-  mouseOverBar = mouseX >= barX && mouseX <= barX + 300 && mouseY >= barY && mouseY <= barY + 50
+	if (holding == true && mouseOverBar) {
+		if (barX + 20 + 5 + barVal + 17.5 < barX + 300 - 10 && go == 'right') {
+			barVal += 3;
+		} else {
+			go = 'left';
+		}
 
-  if(holding == true && mouseOverBar){
-    if(barX + 20 + 5 + barVal + 17.5 < barX + 300 - 10 && go == 'right'){
-      barVal += 3;
-    } else {
-      go = 'left'
-    }
-
-    if(barVal >= 0 && go == 'left'){
-      barVal -= 3;
-    } else {
-      go = 'right'
-    }
-  }
+		if (barVal >= 0 && go == 'left') {
+			barVal -= 3;
+		} else {
+			go = 'right';
+		}
+	}
 
 	//BAR
 	stroke(100);
 	strokeWeight(5);
 	fill(155);
-  rect(barX, barY, 300, 50, 100);
-  
-  spots[0].show();
+	rect(barX, barY, 300, 50, 100);
 
-  //BALL
-  ballX = barX + 20 + 5 + barVal
-  ballY = 200 + 20 + 5
-  fill(200)
-  stroke(120)
-  circle(ballX, ballY, 35)
+	spots[0].show();
 
-  stroke(255, 0, 0)
-  //point((width/2 - 150) + 20 + 5 + barVal + 17.5, 200 + 20 + 5) #DEBUG
+	//BALL
+	ballX = barX + 20 + 5 + barVal;
+	ballY = 200 + 20 + 5;
+	fill(200);
+	stroke(120);
+	circle(ballX, ballY, 35);
 
+	stroke(255, 0, 0);
+	//point((width/2 - 150) + 20 + 5 + barVal + 17.5, 200 + 20 + 5) #DEBUG
 
-  //points = int(map(barVal, 0, 250, 0, 100))
-  yeet = int(map(dist(ballX, ballY, spot.x, spot.y), 0, maxScore, 0, maxScore))
-  
-  points = maxScore - yeet//maxScore - int(dist(ballX, ballY, spot.x, spot.y));
-  if(points < 0){
-    points = 0;
-  }
+	//points = int(map(barVal, 0, 250, 0, 100))
 
-  //print(points)
+	yeet = int(map(dist(ballX, ballY, spot.x, spot.y), 0, maxScore, 0, maxScore));
 
-  //Floating scores
-  for(let i = 0; i < scores.length; i++){
-    scores[i].update()
-    scores[i].show()
-    if(scores[i].dead()){
-      scores.splice(i, 1)
-    }
-  }
+	points = maxScore - yeet; //maxScore - int(dist(ballX, ballY, spot.x, spot.y));
+	if (points < 0) {
+		points = 0;
+	}
 
-  //SCORE
-  textAlign(CENTER, CENTER)
-  textSize(40)
-  fill(255)
-  stroke(0)
-  text(fullScore, width/2, height/2)
+	//print(points)
+
+	//Floating scores
+	for (let i = 0; i < scores.length; i++) {
+		scores[i].update();
+		scores[i].show();
+		if (scores[i].dead()) {
+			scores.splice(i, 1);
+		}
+	}
+
+	//SCORE
+	textAlign(CENTER, CENTER);
+	textSize(40);
+	fill(255);
+	stroke(0);
+	text(fullScore, width / 2, height / 2);
 }
 
-function mousePressed(){
-  if(mouseOverBar){
-    holding = true;
-  }
-
+function mousePressed() {
+	if (mouseOverBar) {
+		holding = true;
+	}
 }
 
-function mouseReleased(){
-  if(mouseOverBar){
-    holding = false;
-    barVal = 0;
-    fullScore += points
-    if(points == maxScore){
-      bestScores++; 
-      scr = new score(mouseX, 200, points, [255, 235, 0], 40, 3);
-      scores.push(scr);
-    }else{
-      bestScores = 0
-      scr = new score(mouseX, 200, points, [225, 225, 0], 20, 5);
-      scores.push(scr);
-    }
-    
-    spot.x = random(barX + 25 + 20, barX + 25 + 250)
-    spot.y = ballY;
-  }
+function mouseReleased() {
+	if (mouseOverBar) {
+		holding = false;
+		barVal = 0;
+		fullScore += points;
+		if (points == maxScore) {
+			bestScores++;
+			scr = new score(mouseX, 200, points, [ 255, 235, 0 ], 40, 3);
+			scores.push(scr);
+		} else {
+			bestScores = 0;
+			scr = new score(mouseX, 200, points, [ 225, 225, 0 ], 20, 5);
+			scores.push(scr);
+		}
+
+		spot.x = random(barX + 25 + 20, barX + 25 + 250);
+		spot.y = ballY;
+	}
 }
 
 //closing
-window.onbeforeunload = function () {
-  saveCookies([maxScore, fullScore, bestScores], ['maxScore', 'fullScore', 'bestScores'], 30);
-}
+/*
+window.onbeforeunload = function() {
+	saveCookies([ maxScore, fullScore, bestScores ], [ 'maxScore', 'fullScore', 'bestScores' ], 30);
+};
+*/
 
 //saving
-setInterval(function(){
-  saveCookies([maxScore, fullScore, bestScores], ['maxScore', 'fullScore', 'bestScores'], 30);
-}, 30 * 1000)
+setInterval(function() {
+	saveCookies([ maxScore, fullScore, bestScores ], [ 'maxScore', 'fullScore', 'bestScores' ], 30);
+}, 30 * 1000);
 
 //save btn
-function pressy(){
-  saveCookies([maxScore, fullScore, bestScores], ['maxScore', 'fullScore', 'bestScores'], 30);
+function pressy() {
+	saveCookies([ maxScore, fullScore, bestScores ], [ 'maxScore', 'fullScore', 'bestScores' ], 30);
 }
